2d051561a70166856a7d95e85ce5118774aaf29b
Hikmal-Yudha <hikmalyudha20@gmail.com>
2025-06-08 14:12:22 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-
-import './App.css';
+import "./App.css";
 
 function Car(props) {
   return <li>I am a {props.brand}</li>;
 }
 
 function Garage() {
-  const cars = ['Ford', 'BMW', 'Audi'];
+  const cars = [
+    { id: 1, brand: "Ford" },
+    { id: 2, brand: "BMW" },
+    { id: 3, brand: "Audi" },
+  ];
   return (
     <>
       <h1>Who live in my garage?</h1>
       <ul>
-      {cars.map((cars) => <Car brand={cars} />)}
+        {cars.map((cars) => (
+          <Car key={cars.id} brand={cars.brand} />
+        ))}
       </ul>
     </>
   );
@@ -19,9 +24,8 @@ function Garage() {
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Garage />
-     
     </div>
   );
 }
